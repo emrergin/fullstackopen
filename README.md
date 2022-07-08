06739731e6f49b2100eea08f2f3edc9e319457e0
emrergin <emrergin2757@yahoo.com>
2022-07-08 09:46:23 +0300

    Exercises 7.4.-7.8.

--- README.md
+++ README.md
@@ -84,13 +84,17 @@ This part is all about Redux. The exercises requested introducing Redux to [unic
 - I used Redux Toolkit for the first time.
 - I learned to use hooks such as `useSelector` and `useDispatch` to access the store.
 - I also learned to use `connect` function to replicate the same functionality.
-- I learned what are Presentational and Container components.
+- I learned what Presentational and Container components are.
 
 ## Part 7 React router, custom hooks, styling app with CSS and webpack
 
-Main bulk of this part was about improving the Bloglist Frontend we made in Part 5. The exercises were detached from the reading material to an extent.
+Main bulk of this part was about improving the Bloglist Frontend we made in *Part 5*. The exercises were detached from the reading material to an extent.
 
 - [Course](https://fullstackopen.com/en/part7)
 - [My solutions](https://github.com/emrergin/fullstackopen/tree/main/part7)
 
 ### What I learned in Part 7
+- Note that, this part required modifications rather than writing from scratch, so most of the code included is not mine.
+- I learned how to use `useNavigation` and `useParams` hooks.
+- I learned some functionality of the Routers can only be used outside of a component.
+- I wrote my first custom hook.
--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -4,6 +4,8 @@ import {
   Routes, Route, Link, useParams, useNavigate
 } from "react-router-dom"
 
+import { useField } from './hooks/index'
+
 const Menu = () => {
   const padding = {
     paddingRight: 5
@@ -52,20 +54,33 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  // const [content, setContent] = useState('')
+  // const [author, setAuthor] = useState('')
+  // const [info, setInfo] = useState('')
+  const content = useField('text');
+  const author = useField('text');
+  const info = useField('text');
+  // const reset = useField('reset');
+
+
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.fields.value,
+      author: author.fields.value,
+      info: info.fields.value,
       votes: 0
     })
     navigate('/')
+  }
+
+  const resetFields = (e) =>{
+    e.preventDefault();
+    content.reset();
+    author.reset();
+    info.reset();
   }
 
   return (
@@ -74,17 +89,20 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input  {...content.fields} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input  {...author.fields} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info.fields} />
         </div>
         <button>create</button>
+        <button onClick={resetFields}>
+          reset
+        </button>
       </form>
     </div>
   )
